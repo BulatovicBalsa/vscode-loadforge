084621665fadfbc83f7ce342570ca4ff36c73eb9
BulatovicBalsa <baki.b.2k02@gmail.com>
2026-03-01 18:00:44 +0100

    feat: Implement runLoadTest command and associated functionality

---
+++ src/commands/run-load-test.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+
+let terminal: vscode.Terminal | null = null;
+
+function getBinaryPath(): string {
+    // TODO: Extend with linux and macOS support
+    const binaryName = 'loadforge.exe';
+    const binaryPath = vscode.extensions.getExtension('siit-na-kvadrat.loadforge')?.extensionPath + '\\bin\\' + binaryName;
+    return binaryPath;
+}
+
+async function collectEnvironmentFilePaths(): Promise<string[]> {
+    const rootPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+    if (!rootPath) {
+        vscode.window.showErrorMessage('No workspace folder found. Please open a folder in VS Code.');
+        return [];
+    }
+    const searchPattern = new vscode.RelativePattern(rootPath, '**/*.env');
+    const files = await vscode.workspace.findFiles(searchPattern);
+    return files.map(file => file.fsPath);
+}
+
+async function promptForEnvironmentFile(envFilePaths: string[]): Promise<string | undefined> {
+    if (envFilePaths.length === 0) {
+        vscode.window.showErrorMessage('No .env files found in the workspace.');
+        return undefined;
+    }
+
+    if (envFilePaths.length === 1) {
+        return envFilePaths[0];
+    }
+
+    const envFileOptions = envFilePaths.map(path => ({ label: vscode.workspace.asRelativePath(path), description: path }));
+    const selectedEnvFile = vscode.window.showQuickPick(envFileOptions, {
+        placeHolder: 'Select an environment file to use for the load test',
+        canPickMany: false
+    });
+    const result = await selectedEnvFile;
+
+    if (result) {
+        // Find the full path corresponding to the selected label
+        const fullPath = envFilePaths.find(path => vscode.workspace.asRelativePath(path) === result.label);
+        return fullPath;
+    }
+    return undefined;
+}
+
+function _runLoadTest(lfFilePath: string, envFilePath: string | undefined) {
+    const args = [lfFilePath];
+    if (envFilePath) {
+        args.push(envFilePath);
+    }
+
+    const binaryPath = getBinaryPath();
+    invokeBinaryExecution(binaryPath, args);
+}
+
+function invokeBinaryExecution(binaryPath: string, args: string[]) {
+    // find terminal with name "LoadForge" or create a new one with debug icon
+    terminal = vscode.window.terminals.find(t => t.name === 'LoadForge') || vscode.window.createTerminal({ name: 'LoadForge', iconPath: new vscode.ThemeIcon('beaker') });
+    terminal.show();
+    const cmd = `& "${binaryPath}" ${args.map(a => `"${a}"`).join(" ")}`;
+    terminal.sendText(cmd);
+}
+
+export async function runLoadTest(lfFilePath: string) {
+    const envFilePaths = await collectEnvironmentFilePaths();
+    const selectedEnvFile = await promptForEnvironmentFile(envFilePaths);
+    _runLoadTest(lfFilePath, selectedEnvFile);
+}
+
+export function stopLoadTest() {
+    terminal?.sendText('\x03'); // Send Ctrl+C to stop the process
+}
