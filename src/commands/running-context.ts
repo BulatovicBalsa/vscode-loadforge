c3f556f92dba7aebb8e3a8277a1b10e8e42351cb
BulatovicBalsa <baki.b.2k02@gmail.com>
2026-03-02 02:24:32 +0100

    feat: Remove information message on load test state update

--- src/commands/running-context.ts
+++ src/commands/running-context.ts
@@ -5,5 +5,4 @@ let isRunning = false;
 export function updateIsRunningState(running: boolean) {
     isRunning = running;
     vscode.commands.executeCommand('setContext', 'loadforge.isRunning', isRunning);
-    vscode.window.showInformationMessage(`Load test is now ${isRunning ? 'running' : 'stopped'}.`);
 }
